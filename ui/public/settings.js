5d611a0eb293bcbc1daf3b34160437d0a669c875
Roberto Prevato <roberto.prevato@gmail.com>
2021-12-10 16:12:03 +0100

    dockerfile

--- ui/public/settings.js
+++ ui/public/settings.js
@@ -1,7 +1,7 @@
 // These settings control the SPA target API, SPA client id, API scope
 // (to obtain access tokens for the API)
 window.Settings = {
-  api_url: "http://localhost:44777",
+  api_url: "",
   api_scope: "",
   client_id: "",
   client_authority: "",
